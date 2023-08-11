edad97d5b6f622011f26072bf1bcd9d4106950aa
NicoDS9688 <nicolasdickel@hotmail.com>
2023-08-10 21:27:29 -0400

    trabajo terminado

--- Core/encuesta_dojo/server.py
+++ Core/encuesta_dojo/server.py
@@ -1,11 +1,24 @@
 """Server"""
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 
 app = Flask(__name__, static_folder='static')
+app.secret_key = "keep it secret, keep it safe"
 
-@app.route('/')
-def users_list():
-    """Método que renderiza la lista de estudiantes"""
+@app.route("/")
+def base():
+    """Método que devuelve el template index"""
+    return render_template("form.html")
+
+@app.route('/process/', methods=['POST'])
+def process():
+    """Método para guardar los datos en cache"""
+    session['name'] = request.form.get('name')
+    session['location'] = request.form.get('location')
+    session['language'] = request.form.get('language')
+    session['comments'] = request.form.get('comments')
+
+    print(request.form)
+    return render_template('result.html')
 
 
 if __name__=="__main__":
